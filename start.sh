b581ac8b8339d414750a8ad7902a79cb989e30c6
carlos <carducaldeira@gmail.com>
2024-05-30 20:41:49 -0300

    update docker for write permissions

--- start.sh
+++ start.sh
@@ -8,6 +8,9 @@ else
   echo "Directory ./ignorefolder already exists."
 fi
 
+export USER_ID=$(id -u)
+export GROUP_ID=$(id -g)
+
 # Inicia o Docker Compose
 docker compose -f docker/docker-compose.yml up -d
 docker compose -f  docker/docker-compose.yml exec python-project bash 
