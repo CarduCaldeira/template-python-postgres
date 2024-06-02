517810cbeb4f3c8564b0d960ca0efdcfc4ae329c
Mateus Oliveira <msouzaol@redhat.com>
2024-06-02 16:23:29 -0300

    fix: documentation examples rendering
    Signed-off-by: Mateus Oliveira <msouzaol@redhat.com>

--- source/app.py
+++ source/app.py
@@ -4,13 +4,15 @@ def add(a:float, b:float) -> float:
     """
     Adiciona dois números e retorna o resultado.
 
-    Exemplo:
-    >>> add(2, 3)
-    5
-    >>> add(-1, 1)
-    0
-    >>> add(0, 0)
-    0
+    Examples:
+        >>> add(2, 3)
+        5
+
+        >>> add(-1, 1)
+        0
+
+        >>> add(0, 0)
+        0
 
     Args:
         a (float): Numero  a ser somado.
